a1ea6262b44bb1badd0f14ee144bb3ea58adbaf3
liamh <liamh@magmadigital.co.uk>
2017-11-26 11:46:40 +0000

    Clean up botman routes and add a Natural Language Processing middleware.

--- routes/botman.php
+++ routes/botman.php
@@ -5,32 +5,16 @@ use BotMan\BotMan\Messages\Outgoing\OutgoingMessage;
 
 $botman = resolve('botman');
 
-$botman->hears('Hi', function ($bot) {
-    $bot->reply('Hello!');
-});
+$dialogflow = \BotMan\BotMan\Middleware\ApiAi::create(env('DIALOGFLOW_TOKEN'))->listenForAction();
+
+$botman->middleware->received($dialogflow);
+
+$botman->hears('joke', BotManController::class.'@tellAJoke')->middleware($dialogflow);
+
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
 
 $botman->hears('Help!', BotManController::class.'@helpUser');
 
 $botman->hears('Create message', BotManController::class.'@createMessage');
-
-$botman->hears('Tony', function ($bot) {
-    $attachment = new Image('https://i.imgur.com/vXlh4ir.jpg');
-
-    $message = OutgoingMessage::create('Do you mean this blert?')
-        ->withAttachment($attachment);
-
-    $bot->reply($message);
-});
-
-$botman->hears('Keith', function ($bot) {
-    $attachment = new Image('https://falsefabs.files.wordpress.com/2013/09/dave-myers.jpg');
-
-    $message = OutgoingMessage::create('Do you mean this blert?')
-        ->withAttachment($attachment);
-
-    $bot->reply($message);
-    $bot->reply('Do you mean this blert?');
-});
 
 $botman->fallback(BotManController::class.'@fallBackMessages');
