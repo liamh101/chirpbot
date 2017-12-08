d078fe1578dd97a4ed03a82606d902a0ab39ccec
liamh <liamh@magmadigital.co.uk>
2017-12-08 19:49:16 +0000

    Add Facebook Photo post artisan command and remove from api endpoint.

---
+++ app/Console/Commands/facebookPhotoPost.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Services\FacebookService;
+use App\Services\RedditService;
+use Illuminate\Console\Command;
+
+class facebookPhotoPost extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'facebook:funnyPhotoPost';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Post the current top post of r/funny to facebook wall.';
+
+    /** @var FacebookService */
+    protected $facebookService;
+
+    /** @var RedditService  */
+    protected $redditService;
+
+    /**
+     * Create a new command instance.
+     *
+     * @param FacebookService $facebookService
+     */
+    public function __construct(FacebookService $facebookService)
+    {
+        parent::__construct();
+        $this->facebookService = $facebookService;
+        $this->redditService = resolve(RedditService::class);
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     * @throws \Exception
+     * @throws \Facebook\Exceptions\FacebookSDKException
+     */
+    public function handle()
+    {
+        $post = $this->redditService->getTopFunnyImage();
+        $this->facebookService->createNewPhotoPost($post['title'], $post['link']);
+    }
+}
